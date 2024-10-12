9909e49e4b9911d6a7d5e7dd754299f1ba7040b7
ritikprajapat21 <ritikprajapati084@gmail.com>
2024-10-12 07:43:52 +0530

    Edited config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/futy-assignment/",
+  base: "futy-assignment",
   plugins: [react()],
   resolve: {
     alias: {
